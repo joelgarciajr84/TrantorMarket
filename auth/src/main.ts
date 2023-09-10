1492ed6a6d2117219b1cce1e891391271d2c77ed
Joel Garcia Jr <joel.garciajr84@gmail.com>
2023-09-10 18:55:45 -0300

    Fix login behaviour

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -1,8 +1,23 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const looger = new Logger('bootsrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+
+  const options = new DocumentBuilder()
+    .setTitle('TRANTOR MARKET AUTH SERVICE')
+    .setDescription('TRANTOR MARKET AUTH SERVICE')
+    .setVersion('1.0')
+    .addTag('v1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(port);
+
+  looger.log(`Auth API running on port ${port}`);
 }
 bootstrap();
