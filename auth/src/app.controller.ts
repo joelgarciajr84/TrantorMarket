f2c4ce2b43703aac5c32d56a273c31ce727c3d33
Joel Garcia Jr <joel.garciajr84@gmail.com>
2023-09-09 23:28:35 -0300

    Adds debug and docker

--- auth/src/app.controller.ts
+++ auth/src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -10,3 +9,4 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
